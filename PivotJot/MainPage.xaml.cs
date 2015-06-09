f7eb446b85342f56f477e1c018e87c5c796b9628
Michael Ensly <michael.ensly@two-bulls.com>
2015-06-09 18:03:10 +1000

    Loading UI

--- PivotJot/MainPage.xaml.cs
+++ PivotJot/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,28 +25,24 @@ namespace PivotJot
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const string TOKEN_DEBUG = "__debug__";
         private const int PAGE_LIST = 0;
         private const int PAGE_JOT = 1;
-        public ObservableCollection<Project> Projects { get; private set; }
+
         private Project selected;
-        public Project Selected
-        {
-            get { return selected; }
-            set
-            {
-                selected = value;
-                titleEntry.IsEnabled = selected != null;
-                TextChanged(titleEntry, null);
-            }
-        }
+        private string token = TOKEN_DEBUG;
+        private bool loadingList;
+        private bool loadingSubmit;
+
+        public ObservableCollection<Project> Projects { get; private set; }
 
         public MainPage()
         {
             this.InitializeComponent();
             Projects = new ObservableCollection<Project>();
-            Projects.Add(new Project(1) { Name = "Test 1" });
-            Projects.Add(new Project(2) { Name = "Test 2" });
-
+            // TODO: Reload projects from cache
+            LoadingList = Projects.Count == 0;
+            LoadProjects();
             this.DataContext = this;
 
             if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
@@ -82,6 +79,67 @@ namespace PivotJot
         private void TextChanged(object sender, TextChangedEventArgs e)
         {
             submitBtn.IsEnabled = Selected != null && titleEntry.Text.Length > 0;
+        }
+
+        private async void LoadProjects()
+        {
+            if (token == TOKEN_DEBUG)
+            {
+                await Task.Delay(2000);
+                Projects.Clear();
+                Projects.Add(new Project(1) { Name = "Test 1" });
+                Projects.Add(new Project(2) { Name = "Test 2" });
+                LoadingList = false;
+            }
+        }
+
+        public Project Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                titleEntry.IsEnabled = selected != null;
+                TextChanged(titleEntry, null);
+            }
+        }
+
+        private void LoadingChanged()
+        {
+            loadingIndicator.Visibility = (loadingList || loadingSubmit) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public bool LoadingList
+        {
+            get { return loadingList; }
+            set { loadingList = value; LoadingChanged(); }
+        }
+        public bool LoadingSubmit
+        {
+            get { return loadingSubmit; }
+            set { loadingSubmit = value; LoadingChanged(); }
+        }
+
+        private async void Submit(object sender, RoutedEventArgs e)
+        {
+            IsEnabled = false;
+            LoadingSubmit = true;
+            if (token == TOKEN_DEBUG)
+            {
+                await Task.Delay(4000);
+            }
+            titleEntry.Text = "";
+            LoadingSubmit = false;
+            IsEnabled = true;
+            titleEntry.Focus(FocusState.Programmatic);
+        }
+
+        private void OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                Submit(sender, new RoutedEventArgs());
+            }
         }
     }
 }
