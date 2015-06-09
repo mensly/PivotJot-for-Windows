442f259f0932b0ae6f721f3e57df203e3461318c
Michael Ensly <michael.ensly@two-bulls.com>
2015-06-09 17:12:58 +1000

    UI for list of projects that navigates to jot screen

--- PivotJot/MainPage.xaml.cs
+++ PivotJot/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,65 @@ namespace PivotJot
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int PAGE_LIST = 0;
+        private const int PAGE_JOT = 1;
+        public ObservableCollection<Project> Projects { get; private set; }
+        private Project selected;
+        public Project Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                titleEntry.IsEnabled = selected != null;
+                TextChanged(titleEntry, null);
+            }
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            Projects = new ObservableCollection<Project>();
+            Projects.Add(new Project(1) { Name = "Test 1" });
+            Projects.Add(new Project(2) { Name = "Test 2" });
+
+            this.DataContext = this;
+
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                Windows.Phone.UI.Input.HardwareButtons.BackPressed += (o, args) => args.Handled = OnBackPressed();
+            }
+
+        }
+
+        private bool OnBackPressed()
+        {
+            if (pivotView.SelectedIndex != PAGE_LIST)
+            {
+                pivotView.SelectedIndex = PAGE_LIST;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async void ProjectSelected(object sender, SelectionChangedEventArgs e)
+        {
+            Selected = e.AddedItems.FirstOrDefault() as Project;
+            if (Selected != null)
+            {
+                pivotView.SelectedIndex = PAGE_JOT;
+                await Task.Delay(250);
+                titleEntry.Focus(FocusState.Programmatic);
+            }
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            submitBtn.IsEnabled = Selected != null && titleEntry.Text.Length > 0;
         }
     }
 }
+
---
+++ PivotJot/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PivotJot
+{
+    public class Project
+    {
+        public int ProjectId { get; private set; }
+        public String Name { get; set; }
+        public List<String> Tags { get; set; }
+        public Project(int id)
+        {
+            ProjectId = id;
+            Tags = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
